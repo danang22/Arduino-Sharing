09e1b3f4f2cc26109532aa20fa5189578110b6c5
danang22 <66998452+danang22@users.noreply.github.com>
2021-01-15 12:52:37 +0700

    Add files via upload

---
+++ blynk_asap_alarm.ino
@@ -0,0 +1,56 @@
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+//#include <SimpleTimer.h>
+#define BLYNK_PRINT Serial
+
+
+const int smoke = A0;
+const int Pinbuzzer = D2;
+
+//int data;
+int dataAsap;
+int data = 500;
+
+char auth[] = "qCPK0kIFkf9XrK2FpT78xX-edOgf7alY";
+
+
+char ssid[] = "Samsung Galaxy A50";
+char pass[] = "danang3776";
+
+SimpleTimer timer;
+
+void setup(){
+  pinMode(smoke, INPUT);
+  pinMode(Pinbuzzer, OUTPUT);
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+  timer.setInterval(1000L, getSendData);
+}
+
+void loop(){
+  
+  timer.run();
+  Blynk.run();
+  int analogSensor = analogRead(smoke);
+  Serial.print("Nilai sensor: ");
+  Serial.println(analogSensor);
+  delay(3000);
+  if (analogSensor > data){
+    tone(Pinbuzzer, 2000, 1000);
+  }
+  else{
+    noTone(Pinbuzzer);
+  }
+}
+
+void getSendData()
+{
+  data = analogRead(smoke);
+  Blynk.virtualWrite(V2, data);
+
+  if (data > 500)
+  {
+    Blynk.notify("Asap Terdeteksi");
+  }
+}
